440efcb5cd7e2bb5490d04dc40679097e03554f5
LimesKey <limeskeys@gmail.com>
2023-12-27 13:52:04 -0500

    no errors

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[tokio::main]
 async fn main() {
     tauri::Builder::default()
@@ -15,12 +9,11 @@ async fn main() {
         .expect("error while running tauri application");
 }
 
-async fn api () {
-    use mc_query::status;
-    use tokio::io::Result;
+use mc_query::status;
+use crate::status::StatusResponse;
 
-    let data = status("mc.hypixel.net", 25565).await?;
-    println!("{data:#?}");
-
-    Ok(())
+#[tauri::command]
+async fn api() -> Result<StatusResponse, Error> {
+    let data = status("mc.hypixel.net", 25565).await;
+    data
 }
