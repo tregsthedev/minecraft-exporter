59a44be208c1acc5aff04aeb9b8c351a5b743a1f
LimesKey <limeskeys@gmail.com>
2023-12-27 15:23:50 -0500

    You won't believe what fixed

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
-    import { invoke } from '@tauri-apps/api/tauri';
-  async function something() {
-    return message = await invoke('api')
+  let message = '';
+  import { invoke } from '@tauri-apps/api/tauri';
+
+  async function fetchMessage() {
+    try {
+      message = await invoke('api');
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
   }
+
+  fetchMessage();
 </script>
 
 <h1>Hello world! {message}</h1>
-
-
