1daf709f803f98a68d821e7890d8b23310a2354c
Arav Narula <arav.1tb@gmail.com>
2023-12-27 17:14:08 -0500

    OMG IT WORKS

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,20 @@
 <script>
   let message = '';
+  let playerCount = 2;
+  let data = '';
+  let serverName = ''
+
   import { invoke } from '@tauri-apps/api/tauri';
 
   async function fetchMessage() {
     try {
-      message = await invoke('api');
+      message = await invoke('api')
+      data = JSON.parse(message)
+      name = data.players.max
+      let {version, players } = data
+
+
+
     } catch (error) {
       console.error('Error occurred:', error);
     }
@@ -14,4 +24,16 @@
   fetchMessage();
 </script>
 
+<div class="header">
+<h1 class="title"> Server: {serverName} </h1>
+
+</div>
+
 <h1>Hello world! {message}</h1>
+<h3> <span class="server-name"> servername </span> </h3>
+<p>
+<span class="player-count"> player count: </span> {playerCount}
+hi
+</p>
+
+<h2> hi {name} </h2>
